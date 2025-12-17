41738c68309f8bd4eb1c56422b0c2e0eafd71355
Irfan Muhaimin <irfanmuhaimin09@gmail.com>
2025-12-17 11:53:28 +0800

    UI new

--- src/pages/JobcardsPage.js
+++ src/pages/JobcardsPage.js
@@ -7,7 +7,7 @@ import SecurityOverlay from '../components/SecurityOverlay';
 import '../styles/DrugManagement.css'; 
 
 // --- DEVELOPMENT SWITCH: Set this to 'false' to disable the Face ID check ---
-const IS_FACE_ID_ENABLED = false;
+const IS_FACE_ID_ENABLED = true;
 
 function JobcardsPage() {
     const { user } = useAuth();
